6e71515d6036e2d894f47aa2dc94cd32b06eaec8
ucefez7 <yousafalieamonu@gmail.com>
2024-03-15 17:45:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  let [todos,setTodos]= useState([])
+  let [todo, setTodo]= useState('')
+
+  let deleteTodo=(id)=>{
+    setTodos(todos.filter((todo)=>todo.id!=id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>To Do App</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Nthelm Plan chyy mwonu </h2>
+      </div>
+      <div className="input">
+
+        <input value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Ntha plan..." />
+        <i onClick={()=>setTodos([...todos, {id: Date.now(), text:todo, status:false}])} className="fas fa-plus"></i>
+
+
+      </div>
+      <div className="todos">
+{
+  todos.map((x)=>{
+    return( 
+         
+        <div className="todo">
+          <div className="left">
+
+            <input onChange={(e)=>{
+              setTodos(todos.filter((y)=>{
+                if(y.id==x.id){
+                  y.status=e.target.checked
+                }
+                return y
+              }))
+            }}
+            value={x.status} type="checkbox" name="" id="" />
+
+            <p>{x.text}</p>
+
+          </div>
+          <div className="right">
+            <i onClick={()=>deleteTodo(x.id)} className="fas fa-times"></i>
+          </div>
+        </div>
+    )
+  })
+}
+
+{todos.map((x)=>{
+  if(x.status){
+    return(
+      <h2>{x.text}</h2>
+    )
+  }
+  return null
+})}
+
+      </div>
     </div>
   );
 }
